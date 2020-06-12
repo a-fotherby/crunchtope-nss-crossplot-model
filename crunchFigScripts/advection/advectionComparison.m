b45794fda88c72be8293c3ea264ae256a85319f8
Angus Fotherby <angusfotherby@gmail.com>
2020-06-12 16:31:26 +0100

    Modified to generate data at runtime, rather than reading from deleted database.

--- crunchFigScripts/advection/advectionComparison.m
+++ crunchFigScripts/advection/advectionComparison.m
@@ -1,34 +1,32 @@
 %% Plot each case's crossplot with and without advection for comparison.
-t = tiledlayout(2,4, 'TileSpacing', 'compact', 'Padding', 'compact');
+t = tiledlayout(2,4)
+oldFolder = pwd;
+cd('~/Dropbox/Academic/Isotope Model/crunchModel/')
 
-for caseNum = [1 2 3 4]
-    nexttile(caseNum);
-    directory = sprintf('~/Dropbox/Academic/Isotope Model/crunchModel/crunchData/case%d', caseNum);
-    cd(directory);
-    isotopeCrossplot('SO4--', 'SO164--');
-    caseLabel = sprintf('Case %d', caseNum);
-    title(gca, caseLabel, 'FontWeight', 'bold')
-    xlabel(gca, '\delta^{34}S')
-    ylabel(gca, '\delta^{18}O')
-    xlim(gca, [20 120])
-    ylim(gca, [8 22])
-    axis square;
-    if caseNum == 1
-        ylabel(gca, {'No advection' '\delta^{18}O'});
+for caseNum = [1:4]
+    nexttile(caseNum)
+
+    runCommand = sprintf('~/soft/crunchtope/CrunchTope case%d.in', caseNum);
+    system(runCommand)
+    if caseNum == 3 || caseNum == 4
+        restartCommand = sprintf('~/soft/crunchtope/CrunchTope case%dRestart.in', caseNum);
+        system(restartCommand)
     end
+    !~/.scripts/cleanCrunchOutput.sh
     
-    nexttile(caseNum + 4);
-    directory = sprintf('~/Dropbox/Academic/Isotope Model/crunchModel/crunchData/case%dAdv', caseNum);
-    cd(directory);
-    isotopeCrossplot('SO4--', 'SO164--');
-    xlabel(gca, '\delta^{34}S')
-    ylabel(gca, '\delta^{18}O')
-    xlim(gca, [20 120])
-    ylim(gca, [8 22])
-    axis square;
-    if caseNum == 1
-        ylabel(gca, {'Advection = 50m/myr' '\delta^{18}O'});
-    end
-end
+    isotopeCrossplot('SO4--', 'SO164--')
+    axis square
+    
+    nexttile(caseNum + 4)
 
-set(findall(gcf,'-property','FontSize'),'FontSize',12)
+    runCommand = sprintf('~/soft/crunchtope/CrunchTope advcase%d.in', caseNum);
+    system(runCommand)
+    if caseNum == 3 || caseNum == 4
+        restartCommand = sprintf('~/soft/crunchtope/CrunchTope advcase%dRestart.in', caseNum);
+        system(restartCommand)
+    end
+    !~/.scripts/cleanCrunchOutput.sh
+    
+    isotopeCrossplot('SO4--', 'SO164--')
+    axis square
+end
