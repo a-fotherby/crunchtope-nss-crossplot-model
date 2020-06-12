2da7772e98cc6224220b236f4f5d99875cc4ef3a
Angus Fotherby <angusfotherby@gmail.com>
2020-06-12 16:10:32 +0100

    Scale testing was a dead end. Deleting files.

--- crunchFigScripts/scaleTesting/nonZeroList.m
+++
@@ -1,9 +0,0 @@
-%% Find first non-zero element each row in a matrix and compile them into an array.
-function outputArray = nonZeroList(inputMatrix)
-    arrayDepth = size(inputMatrix, 1);
-    outputArray = zeros(1, arrayDepth);
-    for i=1:arrayDepth
-        firstNonZero = inputMatrix(i, find(inputMatrix(i, :), 1));
-        outputArray(1, i) = firstNonZero;
-    end
-end
